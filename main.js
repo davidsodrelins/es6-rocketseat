32c8156be89ff198f926956a1705ee3a444fb146
David Sodré Lins <davidsodre_ba@hotmail.com>
2019-07-22 13:29:06 -0300

    documented - create class and add extends - ES6 - Rocketseat Challenge 01

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-class Usuario{
-    constructor (email, senha){
-        this.email = email;
-        this.email = senha;
+class Usuario{ //criação da classe Usuario
+    constructor (email, senha){ //método construtor recebe email e senha
+        this.email = email; //atribuição do paramento da classe ao da função
+        this.email = senha; 
     }
 
-    isAdmin(){
-        return this.admin ===true;
-    }
-}
-
-class Admin extends Usuario{
-    constructor(email, senha){
-        super(email, senha);
-        this.admin = true;
+    isAdmin(){ // método que verifica se o usuário é um admin
+        return this.admin ===true; // retorno true caso o admin seja true
     }
 }
 
-const User1 = new Usuario('email@teste.com', 'senha123');
-const Adm1 = new Admin('email@teste.com', 'senha123');
+class Admin extends Usuario{ // classe admin herda de usuario
+    constructor(email, senha){ // construtor de admin tbm recebe email e senha
+        super(email, senha); //super para pegar os atributos e métodos da classe pai
+        this.admin = true; //parametro de admin é true pois o é um Admin
+    }
+}
 
-console.log(User1.isAdmin());
-console.log(Adm1.isAdmin()) ;
+const User1 = new Usuario('email@teste.com', 'senha123'); // instancia de usuario
+const Adm1 = new Admin('email@teste.com', 'senha123'); // instancia de admin
+
+console.log(User1.isAdmin()); //retorna se o usuario é admin - false
+console.log(Adm1.isAdmin()) ; //retorna se o admin é admin = true

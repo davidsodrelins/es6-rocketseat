7f4d362314850dc3570119623d30d6543160aab2
David Sodré Lins <davidsodre_ba@hotmail.com>
2019-07-22 13:24:08 -0300

    create class and add extends - ES6 - Rocketseat Challenge 01

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
-alert("teste");
+class Usuario{
+    constructor (email, senha){
+        this.email = email;
+        this.email = senha;
+    }
 
+    isAdmin(){
+        return this.admin ===true;
+    }
+}
 
+class Admin extends Usuario{
+    constructor(email, senha){
+        super(email, senha);
+        this.admin = true;
+    }
+}
+
+const User1 = new Usuario('email@teste.com', 'senha123');
+const Adm1 = new Admin('email@teste.com', 'senha123');
+
+console.log(User1.isAdmin());
+console.log(Adm1.isAdmin()) ;

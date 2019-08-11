d10e7066b427f28703cc62de3ee874ae2d2565b9
David Sodré Lins <davidsodre_ba@hotmail.com>
2019-08-11 15:09:51 -0300

    Questao 5 - ES6 Rocket Seat)

--- bundle.js
+++ bundle.js
@@ -1,40 +1,70 @@
 "use strict";
 
-/**4.1 Desestruturação simples */
-var empresa = {
-  nome: 'Rocketseat',
-  endereco: {
-    cidade: 'Rio do Sul',
-    estado: 'SC'
-  }
-};
-var nome = empresa.nome,
-    cidade = empresa.endereco.cidade,
-    estado = empresa.endereco.estado;
-console.log(nome); // Rocketseat
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { keys.push.apply(keys, Object.getOwnPropertySymbols(object)); } if (enumerableOnly) keys = keys.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); return keys; }
 
-console.log(cidade); // Rio do Sul
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
-console.log(estado); // SC
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 /**
- * Na seguinte função:
-function mostraInfo(usuario) {
- return `${usuario.nome} tem ${usuario.idade} anos.`;
-}
-mostraInfo({ nome: 'Diego', idade: 23 })
-Utilize a desestruturação nos parâmetros da função para buscar o nome e idade do usuário
-separadamente e a função poder retornar apenas:
-return `${nome} tem ${idade} anos.`; 
-*/
+ * 5.1 Rest (pegar o resto das propriedades)
+A partir do array: const arr = [1, 2, 3, 4, 5, 6], defina uma variável
+ x que recebe a primeira posição do vetor e outra variável y
+  que recebe todo restante dos dados.
+ */
+var arr = [1, 2, 3, 4, 5, 6];
+var x = arr[0],
+    y = arr.slice(1); //x é o primeiro e y o resto
 
-function mostraInfo(_ref) {
-  var nome = _ref.nome,
-      idade = _ref.idade;
-  return "".concat(nome, " tem ").concat(idade, " anos.");
+console.log(x); // 1
+
+console.log(y); // [2, 3, 4, 5, 6]
+
+/**
+ * Crie uma função que recebe inúmeros parâmetros e retorna a soma de todos eles:
+ */
+
+function soma() {
+  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
+    params[_key] = arguments[_key];
+  }
+
+  return params.reduce(function (total, next) {
+    return total + next;
+  });
 }
 
-console.log(mostraInfo({
+console.log(soma(1, 2, 3, 4, 5, 6)); // 21
+
+console.log(soma(1, 2)); // 3
+
+/**
+ * A partir do objeto e utilizando o operador spread:
+ */
+
+var usuario = {
   nome: 'Diego',
-  idade: 23
-}));
+  idade: 23,
+  endereco: {
+    cidade: 'Rio do Sul',
+    uf: 'SC',
+    pais: 'Brasil'
+  }
+};
+/**
+Crie uma variável usuario2 que contenha todos os dados do usuário porém com nome Gabriel.
+Crie uma variável usuario3 que contenha todos os dados do usuário porém com cidade Lontras.
+ */
+
+var usuario2 = _objectSpread({}, usuario, {
+  nome: 'Gabriel'
+});
+
+var usuario3 = _objectSpread({}, usuario, {
+  endereco: {
+    cidade: 'Lontras'
+  }
+});
+
+console.log(usuario2);
+console.log(usuario3);

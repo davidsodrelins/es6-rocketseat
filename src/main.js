a16b96fe0d3da5430f3f59fcfdacf3ee906c6e92
David Sodré Lins <davidsodre_ba@hotmail.com>
2019-08-11 17:28:05 -0300

    Questao 1 - ES6 - Modulo 2 - Rocket Seat)

---
+++ src/main.js
@@ -0,0 +1,23 @@
+//Questao 1.1
+import ClasseUsuario from './function'
+ClasseUsuario.info();
+
+//Questao 1.2
+import {idade} from './function'
+console.log(idade);
+
+//Questao 1.3
+//import Usuario, {IdadeUsuario as idade} from './function';
+
+
+
+
+
+
+
+
+
+
+
+
+
